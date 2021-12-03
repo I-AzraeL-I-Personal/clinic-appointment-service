2986864a243341398820a25ce5e0c8d3eca8eef0
I-AzraeL-I <qaraqan70@gmail.com>
2021-12-03 12:02:51 +0100

    Update feign service name

--- src/main/java/com/mycompany/appointmentservice/service/DoctorClient.java
+++ src/main/java/com/mycompany/appointmentservice/service/DoctorClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient("doctor-server")
+@FeignClient("${doctorServiceName}")
 public interface DoctorClient {
 
     @GetMapping(value = "/{uuid}")
